6f47a01e4e89dc5307f5b53a2e72158e80221c0a
Izac626 <dkdlwkr207@gmail.com>
2026-03-18 18:39:37 +0900

    feat: 회원가입 기반 구조 설정 및 DB 연결 설정

--- src/main/java/com/hab/hobbymarket/dao/MemberDAO.java
+++ src/main/java/com/hab/hobbymarket/dao/MemberDAO.java
@@ -1,4 +1,12 @@
 package com.hab.hobbymarket.dao;
 
-public class MemberDAO {
+import com.hab.hobbymarket.model.Member;
+
+public interface MemberDAO {
+
+    // 아이디 중복 체크 (있으면 true, 없으면 false)
+    boolean existsByLoginId(String loginId);
+
+    // 회원 저장
+    void save(Member member);
 }
--- src/main/java/com/hab/hobbymarket/model/MemberSignUpRequest.java
+++ src/main/java/com/hab/hobbymarket/model/MemberSignUpRequest.java
@@ -1,4 +1,42 @@
 package com.hab.hobbymarket.model;
 
 public class MemberSignUpRequest {
+
+    private String loginId;
+    private String password;
+    private String email;
+    private String nickname;
+    private String name;
+    private String phone;
+
+    // 생성자
+    public MemberSignUpRequest(String loginId, String password, String email,
+                               String nickname, String name, String phone) {
+        this.loginId  = loginId;
+        this.password = password;
+        this.email    = email;
+        this.nickname = nickname;
+        this.name     = name;
+        this.phone    = phone;
+    }
+
+    // Getter
+    public String getLoginId()  {
+        return loginId;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getEmail()    {
+        return email;
+    }
+    public String getNickname() {
+        return nickname;
+    }
+    public String getName()     {
+        return name;
+    }
+    public String getPhone()    {
+        return phone;
+    }
 }
