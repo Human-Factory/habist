7fa9562b966fa80a4c282f1c3bbcb60985d319ab
Izac626 <dkdlwkr207@gmail.com>
2026-03-19 12:13:47 +0900

    로그인 구현 끝

--- src/main/java/com/hab/hobbymarket/controller/MemberController.java
+++ src/main/java/com/hab/hobbymarket/controller/MemberController.java
@@ -1,4 +1,20 @@
 package com.hab.hobbymarket.controller;
 
+import com.hab.hobbymarket.model.MemberSignUpRequest;
+import com.hab.hobbymarket.service.MemberService;
+import com.hab.hobbymarket.view.MemberView;
+
 public class MemberController {
+
+    private MemberService memberService = new MemberService();
+    private MemberView memberView = new MemberView();
+
+    public void signUp() {
+
+
+        MemberSignUpRequest request = memberView.getSignUpInput();
+
+
+        memberService.signUp(request);
+    }
 }
