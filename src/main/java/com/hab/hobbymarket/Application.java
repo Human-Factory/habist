e4d6992e694f9dfd9b316394afe9df8d9f2cbdf4
jaewon9804 <kumjaewon9804@g.eulji.ac.kr>
2026-03-19 15:12:45 +0900

    팀원 변경사항 pull

--- src/main/java/com/hab/hobbymarket/Application.java
+++ src/main/java/com/hab/hobbymarket/Application.java
@@ -5,38 +5,5 @@ import com.hab.hobbymarket.view.LoginView;
 import java.util.Scanner;
 
 public class Application {
-    public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        MemberController memberController = new MemberController();
-        LoginView loginView = new LoginView();
-
-        while (true) {
-            System.out.println("\n===== HABIS 메뉴 =====");
-            System.out.println("1. 회원가입");
-            System.out.println("2. 회원 탈퇴");
-            System.out.println("3. 로그인");
-            System.out.println("0. 종료");
-            System.out.print("선택: ");
-
-            int menu = sc.nextInt();
-
-            switch (menu) {
-                case 1:
-                    memberController.signUp();
-                    break;
-                case 2:
-                    memberController.deactivateMember();
-                    break;
-                case 3:
-                    loginView.login();
-                    break;
-                case 0:
-                    System.out.println("프로그램 종료");
-                    return;
-                default:
-                    System.out.println("잘못된 입력입니다.");
-            }
-        }
-    }
 }
