48501bd51d37485944c861bd31d9bee9bcfd7e00
mnppi223 <mnppi223@gmail.com>
2026-03-19 11:39:07 +0900

    testing ContentController
    카테고리 목록 뜨게 하는 것 까지 성공했습니다!

--- src/main/java/com/hab/global/config/DBTestingApplication.java
+++ src/main/java/com/hab/global/config/DBTestingApplication.java
@@ -1,5 +1,10 @@
-package com.hab.global.config;
 
+/* comment.
+    DB가 잘 구동되는지 확인하기 위해서 만들어진 Testing
+    파일입니다. 정상 작동 하는 과정 확인했습니다.
+ */
+
+package com.hab.global.config;
 
 import java.sql.Connection;
 
--- src/main/java/com/hab/hobbymarket/Application.java
+++ src/main/java/com/hab/hobbymarket/Application.java
@@ -1,7 +1,12 @@
 package com.hab.hobbymarket;
 
+import com.hab.hobbymarket.controller.ContentController;
+
 public class Application {
     public static void main(String[] args) {
+
+        ContentController controller = new ContentController();
+        controller.showCategory();
 
     }
 }
