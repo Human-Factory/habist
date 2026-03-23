45b61d39bf6ed5a91f922b8f6a685f6c6e6eb27c
dongdong <105200946+Izac626@users.noreply.github.com>
2026-03-23 23:58:51 +0900

    Delete src directory

--- src/main/java/com/hab/hobbymarket/service/LoginResult.java
+++
@@ -1,8 +0,0 @@
-package com.hab.hobbymarket.service;
-
-public enum LoginResult {
-    SUCCESS,               // 로그인 성공
-    INVALID_CREDENTIAL,    // 아이디 또는 비밀번호 오류
-    LOCKED,                // 계정 잠금
-    INACTIVE               // 비활성 계정
-}
--- src/main/java/com/hab/hobbymarket/view/myinformationview/MyInfomationOutputView.java
+++
@@ -1,4 +0,0 @@
-package com.hab.hobbymarket.view.myinformationview;
-
-public class MyInfomationOutputView {
-}
