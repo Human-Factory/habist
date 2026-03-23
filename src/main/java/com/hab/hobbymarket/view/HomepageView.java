c17b2a30f0d10de751eb67f56e93ed0a48a89143
jaewon9804 <kumjaewon9804@g.eulji.ac.kr>
2026-03-23 11:04:44 +0900

    홈페이지 기능 구현 -> 강의 조회, 로그아웃 기능 연동 // 마이페이지는 추후 구현 예정

--- src/main/java/com/hab/hobbymarket/view/HomepageView.java
+++ src/main/java/com/hab/hobbymarket/view/HomepageView.java
@@ -1,11 +1,16 @@
 package com.hab.hobbymarket.view;
 
 import com.hab.hobbymarket.controller.ContentController;
+import com.mysql.cj.log.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Scanner;
 
 public class HomepageView {
 
+    private static final Logger log = LoggerFactory.getLogger(HomepageView.class);
+    LoginView loginView = new LoginView();
     ContentController content = new ContentController();
     Scanner sc = new Scanner(System.in);
 
@@ -17,6 +22,7 @@ public class HomepageView {
             System.out.println("===========================");
             System.out.println("1. 강의 조회");
             System.out.println("2. 마이페이지");
+            System.out.println("3. 로그아웃");
             System.out.println("0. 종료");
             System.out.println("===========================");
             System.out.print("메뉴를 선택해주세요 : ");
@@ -25,11 +31,17 @@ public class HomepageView {
 
             switch (no) {
                 case "1" ->
-                        content.showLecturesByCategory();
+                    content.showLecturesByCategory();
 
 
 //                case "2" ->
                         // 마이페이지 기능 추가 예정
+
+                case "3" -> {
+                    System.out.println("로그아웃 합니다");
+                    loginView.logout();
+                    return;
+                }
 
                 case "0" -> {
                     System.out.println("프로그램을 종료합니다.");
